482bf52bba59c596410dad598dabb7b466d7bf9d
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-15 18:49:00 +0100

    💄 Change font-family

--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300&display=swap');
+
   *,
   *::after,
   *::before {
@@ -9,6 +11,7 @@ export const GlobalStyles = createGlobalStyle`
   
   body {
     background: ${({ theme }) => theme.colors.body};
+    font-family: 'roboto';
     color: hsl(192,100%,9%);
     font-size: 1.15rem;
     margin: 0;

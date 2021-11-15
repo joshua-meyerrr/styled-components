64f89594cffb0c65ea398585afb4802a439f6baa
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-11-15 18:49:12 +0100

    ✨ Add header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { Container } from "./styles/Container.styled"
-import { StyledHeader, Nav, Logo } from "./styles/Header.styled"
+import { Flex } from "./styles/Flex.styled"
+import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled"
 import { Button } from "./styles/Button.styled"
 
 export default function Header() {
@@ -10,6 +11,20 @@ export default function Header() {
           <Logo src="./images/logo.svg" alt="" />
           <Button>Try It Free</Button>
         </Nav>
+        <Flex>
+          <div>
+            <h1>Build the Community Your Fans Will Love</h1>
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+            <Button bg="#ff0099" color="#fff">
+              Get Started for Free
+            </Button>
+          </div>
+          <Image src="./images/illustration-mockups.svg" alt="" />
+        </Flex>
       </Container>
     </StyledHeader>
   )
